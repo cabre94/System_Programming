df70a85d7c04bdbd1922657cb12bf29991e16791
Cabre <cabre94@hotmail.com>
2022-02-21 00:26:32 -0300

    Terminado

---
+++ Practicas/Practica_5_Sockets/09/UDTs.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "UDTs.h"
+
+
+void init_Req(Req_t* pReq, int id_){
+	pReq->r_id = id_;
+
+	pReq->times.ms_A = (rand() % TIME_RANGE) + 1000;
+	pReq->times.ms_B = (rand() % TIME_RANGE) + 1000;
+	pReq->times.ms_C = (rand() % TIME_RANGE) + 1000;
+}
+
+void print_id(Req_t* pReq){
+	printf("Request #%d\n", pReq->r_id);
+}
+
+
+void printActions(Req_t* req){
+	printf("Request #%d\n", req->r_id);
+}
+
+void print_idWTimes(Req_t* pReq){
+	printf("Request #%d: -->", pReq->r_id);
+	printf(" A: %lfs", pReq->times.ms_A / 1000.0);
+	printf(" B: %lfs", pReq->times.ms_B / 1000.0);
+	printf(" C: %lfs", pReq->times.ms_C / 1000.0);
+	printf(" Total: %lfs\n",
+		  (pReq->times.ms_A+pReq->times.ms_B+pReq->times.ms_C) / 1000.0);
+}
+
+
---
+++ Practicas/Practica_5_Sockets/09/UDTs.h
@@ -0,0 +1,26 @@
+#ifndef _UDTS_H
+#define _UDTS_H
+
+#include <stdio.h>
+#include <string.h>
+
+
+#define TIME_RANGE 1000
+
+typedef struct {
+	int ms_A, ms_B, ms_C;
+}Data_t;
+
+typedef struct{ 
+	int r_id;
+	Data_t times;
+}Req_t;
+
+void init_Req(Req_t* pReq, int r_id_);
+void print_id(Req_t* pReq);
+void print_idWTimes(Req_t* pReq);
+
+
+
+
+#endif // !_UDTS_H
